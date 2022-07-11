2da86163e97312d87d26672c435975d9558f707a
chitrashalini <109006879+chitrashalini@users.noreply.github.com>
2022-07-11 11:43:43 +0530

    function assignment6.1

---
+++ shalini/funsum.py
@@ -0,0 +1,17 @@
+import numbers
+
+
+def count_num():
+    numbers =[10,20,30]
+    sum = 0 
+    for  num in numbers: 
+        sum += num 
+    print(f"Total number is {sum}")
+count_num()
+
+
+
+
+
+
+
