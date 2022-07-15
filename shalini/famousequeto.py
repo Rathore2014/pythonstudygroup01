d926c91932e075b0b435c59aee60b588d99675c8
chitrashalini <109006879+chitrashalini@users.noreply.github.com>
2022-07-14 19:00:41 -0700

    Delete famousequeto.py

--- shalini/famousequeto.py
+++
@@ -1,5 +0,0 @@
-'''famouse queto'''
-famouseperson_1 = "Albert Einsteine said ,"
-massage ="A person who never made mistake never tried anything new ."
-famousequeto = f"{famouseperson_1} {massage}"
-print(famousequeto)
