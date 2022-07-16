66db4163ee8c3ddd82f0aa359c4877b8c16cf312
chitrashalini <109006879+chitrashalini@users.noreply.github.com>
2022-07-16 05:10:56 -0700

    del

--- anjali/funoddeven.py
+++
@@ -1,9 +0,0 @@
-def oddeven(num):
-    if num % 2==0:
-        res ="even"
-    else :   
-        res ="odd" 
-    return res
-num =int(input("enter anumber"))          
-res = oddeven(num)
-print(res)
