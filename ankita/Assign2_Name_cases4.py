728ec2e8636dc8ff84630c84ddd3c3afeb52651f
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-17 16:39:34 +0530

    Upper case,  Lower case, Title case

---
+++ ankita/Assign2_Name_cases4.py
@@ -0,0 +1,10 @@
+#  2-4........ Name Cases: Use a variable to represent a person’s name,
+#             and then print that person’s name in lowercase, uppercase,
+#             and title case.
+
+person_name = "AnKiTa"
+print(person_name.lower())
+print(person_name.upper())
+t = person_name.title()
+print(t)
+
