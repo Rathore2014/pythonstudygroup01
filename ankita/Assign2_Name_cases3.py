1ea9980b23bedb82d3ff32d03c5a748b543ebb4e
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-17 16:24:32 +0530

    Print a message to person using variables

---
+++ ankita/Assign2_Name_cases3.py
@@ -0,0 +1,9 @@
+# 2-3....... Personal Message: Use a variable to represent a person’s name, 
+#            and print a message to that person. Message should be simple, 
+#            such as, “Hello Eric, would you like to learn some
+#            Python today?”
+
+person = "Eric"
+print(person)
+new_person = "Hello Eric, would you like to learn some Python today?"
+print(new_person)
