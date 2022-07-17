2be952dcd96da9d67a1c488bad0729d53ebb5ee6
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-17 17:09:13 +0530

    Compose the message using Third variable

---
+++ ankita/Assign2_Name_cases6.py
@@ -0,0 +1,9 @@
+# 2-6......... Famous Quote 2: Repeat Exercise 2-5, but this time, 
+#              represent the famous person’s name using a variable 
+#              called famous_person. Then compose message and represent
+#              it with a new variable called message. Print message.
+
+famous_person = "A P J Abdul Kalam said,"
+Quote = "Failure will never overtake me if my determination to succeed is strong enough."
+message = f"{famous_person} {Quote}"
+print(message)
