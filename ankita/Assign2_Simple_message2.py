b2ccb3587f0a7a083be143e765035a8751fb7f5a
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-17 16:07:38 +0530

    print the value of variable after change the value

---
+++ ankita/Assign2_Simple_message2.py
@@ -0,0 +1,8 @@
+# 2-2. ........Simple Messages: Assign a message to a variable, 
+#             and print that message.Then change the value of the
+#             variable to a new message, and print the new message. 
+
+simple_mess = "Hello"
+print(simple_mess)
+simple_mess = "10 is greater than 5"
+print(simple_mess)
