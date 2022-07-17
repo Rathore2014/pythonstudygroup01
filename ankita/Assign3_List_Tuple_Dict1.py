76cf3ebcd8f58cc05ce71df5de4776b64c6e5018
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-17 18:03:01 +0530

    Using List, Tuple and Dictionary creating users

---
+++ ankita/Assign3_List_Tuple_Dict1.py
@@ -0,0 +1,25 @@
+# 3-1......... Names: Store the names of One View users. 
+#              Print each user’s name by accessing each user, one at a time.
+# Using list
+users = ["Ankita","Alka","Anjali","Shalini","Aswini"]
+print(users[0])
+print(users[1])
+print(users[2])
+print(users[3])
+print(users[4])
+# Using Tuple
+user = ("Ram","Shyam","Mohan","Sohan","Amit")
+print(user[0])
+print(user[1])
+print(user[2])
+print(user[3])
+print(user[4])
+# Using Dictionary
+user1 = {
+    "l" : "list",
+    "T" : "Tuple",
+    "D" : "Dict"
+    }
+print(user1["l"])
+print(user1["T"])
+print(user1["D"])    
