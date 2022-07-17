cf11ec1815f51c8080bd2b088bfdcd3b87630e64
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-17 16:55:06 +0530

    Print the quote and the name of author using 2 var

---
+++ ankita/Assign2_Name_cases5.py
@@ -0,0 +1,7 @@
+#  2-5......... Famous Quote: Find a quote from a famous person you admire.
+#              Print the quote and the name of its author. Output should 
+#              look something like the following, including the quotation marks:
+
+Author = "A P J Abdul Kalam said,"
+Quote = "If you want to shine like a sun, first burn like a sun."
+print(f"{Author} {Quote}")
