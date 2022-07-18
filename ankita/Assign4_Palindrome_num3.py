37397b27eebd13eef2995aa374257a99102f3d33
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-18 16:32:40 +0530

    Palindrome number using While loop and if condition

---
+++ ankita/Assign4_Palindrome_num3.py
@@ -0,0 +1,13 @@
+# 3..............Palindrome number : WAP to accept a number and check for palindrome.
+
+n=int(input("Accept a number: "))
+temp=n
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+if(temp==rev):
+    print("The number is a palindrome: ")
+else:
+    print("The number is not a palindrome: ")
