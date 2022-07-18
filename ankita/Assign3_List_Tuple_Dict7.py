2fa9d84c8a08fe6cc270fc5eb4008e174d4ee789
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-18 16:23:01 +0530

    Len function in List, Tuple and Dictionary

---
+++ ankita/Assign3_List_Tuple_Dict7.py
@@ -0,0 +1,21 @@
+#  3-7......... Number of networks: Working with one of the programs from 
+#              Exercises 3-6, use len() to print a message indicating 
+#              the number of networks are available.
+
+# Using List
+num = [5,1,8,3,15,21,63,12]
+print(len(num))
+
+# Using Tuple
+nums = (5,1,8,3,21,63,12)
+print(len(nums))
+
+# Using Dictionary
+numb = {
+    2:"1231",
+    4:"3213",
+    5:"1632",
+    1:"4524",
+    3:"2131"
+}
+print(len(numb))
