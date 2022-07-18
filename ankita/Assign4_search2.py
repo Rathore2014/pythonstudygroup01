98be8d0f1a5fb0d06cab0f2d32e6479a2f29bad3
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-18 16:30:14 +0530

    Using for loop search the value in dictionary

---
+++ ankita/Assign4_search2.py
@@ -0,0 +1,10 @@
+# 2.........Search: WAP to find given search value from dictionary values.
+#                   Given dictionary with values
+#                   users = {‘u1’:‘Administrator’, ‘u2’:‘ServerAdmin’, ‘u3’:‘NetworkAdmin’}
+
+users = {'u1':'Administrator', 'u2':'ServerAdmin', 'u3':'NetworkAdmin'}
+for i in users.keys():
+ print(i)
+
+for j in users.values():
+ print(j)
