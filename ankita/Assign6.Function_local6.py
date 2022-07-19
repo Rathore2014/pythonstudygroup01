ea8beb03a88eb7beecd6e6b167309ac35a55cc08
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-19 16:12:46 +0530

    Local variables in function

---
+++ ankita/Assign6.Function_local6.py
@@ -0,0 +1,6 @@
+# 6-5. ...........Variables: Write a function to detect the number of local 
+#                 variables declared in a function.
+def sum(x,y):
+ sum = x + y
+ return sum
+print(sum(5, 10))
