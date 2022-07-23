c825a024b6597d828bd1f7fbecdb817172e00058
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-23 17:52:17 +0530

    Electronic class

--- ankita/Assign8_Class_Electronics1.py
+++ ankita/Assign8_Class_Electronics1.py
@@ -1,13 +1,13 @@
 #  1..........Electronics: 
-#  User Story: As a consumer, I want to be able to organize the tech specs of a Electronics, So that
-# I can make more informed decisions when purchasing a Electronics.
-#  Acceptance Criteria: Given the Electronics class, When I instantiate a new Electronics
-# object, Then the object has the correct properties as described in the assumptions and
-# method to test supported features and print all supported features
-#  Assumptions: The default Electronics will have 8 cpu cores, 8 GB of memory and
-# supports features such as &#39;Wi-Fi&#39;, &#39;Bluetooth&#39;, &#39;Speakers&#39;.
-# o Method to test feature which will take input and returns True or False.
-# o Method to print all supported features
+          #  User Story: As a consumer, I want to be able to organize the tech specs of a Electronics, So that
+          # I can make more informed decisions when purchasing a Electronics.
+          #  Acceptance Criteria: Given the Electronics class, When I instantiate a new Electronics
+          # object, Then the object has the correct properties as described in the assumptions and
+          # method to test supported features and print all supported features
+          #  Assumptions: The default Electronics will have 8 cpu cores, 8 GB of memory and
+          # supports features such as &#39;Wi-Fi&#39;, &#39;Bluetooth&#39;, &#39;Speakers&#39;.
+          # o Method to test feature which will take input and returns True or False.
+          # o Method to print all supported features
 
 class Electeronics():
   processor = '8 gb octacore'
