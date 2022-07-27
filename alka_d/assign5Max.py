2bff03248f8ca5d78274ff3eec122b8e944b01bb
Alka Ragani <109142333+sysapptime@users.noreply.github.com>
2022-07-27 18:29:46 +0530

    Add files via upload
    looping assignment 5

---
+++ alka_d/assign5Max.py
@@ -0,0 +1,17 @@
+# 1......largest number from given list [10,20,30,40,50]
+# list = [10,20,30,40,50]
+# largest_number = list[0]
+# for i in list :
+#     if i > largest_number :
+#         largest_number = i
+# print("largest number in list is",largest_number)        
+
+
+
+#2......wap to find given search value from dictionary value. Given dictionary value are
+#           user = {'u1':'Administrator' , 'u2':'serverAdmin' ,'u3':,NetworkAdmin'}
+users = {'u1':'Administrator' , 'u2':'ServerAdmin' , 'u3':'NetworkAdmin'}
+for i in users.keys():
+    print(users[i])
+for j in users.values():
+    print(j)  
