ee94fddf98e0a229a89ab29a13d4c57842845296
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-08-03 12:41:42 +0530

    Pip install latest version of packages

---
+++ ankita/Assign13_Pip2.py
@@ -0,0 +1,11 @@
+# 13-2. Using pip install latest version of any five packages.
+
+C:\Users\satyam agencies>pip list --uptodate packages
+Package          Version
+--------         -------
+
+pip              22.2.1
+PyMySQL          1.0.2
+six              1.16.0
+sqlparse         0.4.2
+Pillow           9.0.0
