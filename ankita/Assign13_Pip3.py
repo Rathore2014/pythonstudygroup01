f5b4752467e74f697f858c46ed04dab12be97df9
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-08-03 12:57:55 +0530

    Using Pip downgrade packages of old version

---
+++ ankita/Assign13_Pip3.py
@@ -0,0 +1,16 @@
+
+# 13-3. Using pip downgrade all packages to older version.
+C:\Users\satyam agencies>pip list --outdated
+Package                Version Latest  Type
+---------------------- ------- ------- -----
+asgiref                3.4.1   3.5.2   wheel
+distlib                0.3.4   0.3.5   wheel
+Django                 4.0     4.0.6   wheel
+filelock               3.4.2   3.7.1   wheel
+mysql-connector-python 8.0.29  8.0.30  wheel
+Pillow                 9.0.0   9.2.0   wheel
+platformdirs           2.4.1   2.5.2   wheel
+pytz                   2021.3  2022.1  wheel
+setuptools             58.1.0  63.4.0  wheel
+tzdata                 2021.5  2022.1  wheel
+virtualenv             20.12.1 20.16.2 wheel
