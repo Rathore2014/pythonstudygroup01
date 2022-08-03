3706cd7763a4295b580cb1f691bca2f7ac8c0403
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-08-03 14:49:00 +0530

    Using pip install packages from github

---
+++ ankita/Assign13_Pip4.py
@@ -0,0 +1,32 @@
+# 13-4.......... Using pip install package from github.
+
+C:\Users\satyam agencies>pip install https://github.com/Rathore2014/pythonstudygroup01.git
+Collecting https://github.com/Rathore2014/pythonstudygroup01.git
+  Downloading https://github.com/Rathore2014/pythonstudygroup01.git
+     | 169.7 kB 200.6 kB/s 0:00:00
+
+C:\Users\satyam agencies>pip freeze
+asgiref==3.4.1
+cycler==0.11.0
+distlib==0.3.4
+Django==4.0
+filelock==3.4.2
+files==1.1.1
+fonttools==4.34.4
+kiwisolver==1.4.4
+matplotlib==3.5.2
+mysql-connector-python==8.0.29
+numpy==1.23.1
+packaging==21.3
+pandas==1.4.3
+Pillow==9.0.0
+platformdirs==2.4.1
+PyMySQL==1.0.2
+pyparsing==3.0.9
+python-dateutil==2.8.2
+pytz==2021.3
+scipy==1.9.0
+six==1.16.0
+sqlparse==0.4.2
+tzdata==2021.5
+virtualenv==20.12.1
